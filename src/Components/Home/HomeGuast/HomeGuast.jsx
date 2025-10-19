aed12015735d6981c99353738aba1945b3c90929
2323232323233232 <fadmohammed290@gmail.com>
2025-10-20 00:26:00 +0300

    rrrrr

--- src/Components/Home/HomeGuast/HomeGuast.jsx
+++ src/Components/Home/HomeGuast/HomeGuast.jsx
@@ -1,7 +1,31 @@
+// "use client"; 
+// import React from "react";
+// import PromoCard from "@/Components/ui/PromoCard";
+// import GuastAvailable from "./GuastAvailable";
+
+
+// const HomeGuast = () => {
+
+//   return (
+//     <div className="md:max-w-7xl mx-auto pt-2 pb-10 px-4">
+//       {/* Other content can go here */}
+//       <PromoCard />
+
+
+//       {/* Cars Available */}
+//       <GuastAvailable />
+//     </div>
+//   );
+// }
+
+// export default HomeGuast;
+
 "use client"; 
 import React from "react";
 import PromoCard from "@/Components/ui/PromoCard";
-import GuastAvailable from "./guastAvailable";
+import GuastAvailable from "./GuastAvailable";
+
+
 
 const HomeGuast = () => {
 
